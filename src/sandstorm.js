dbb5132ae851feb83aae7e9540d0531f92514611
Michał Żaloudik <ponury.kostek@gmail.com>
2018-04-13 10:48:38 +0200

    fixed `collection.createIndex` params

--- src/sandstorm.js
+++ src/sandstorm.js
@@ -240,7 +240,12 @@ function _ensure_indexes(db, schemas) {
 					if (typeof index.fieldOrSpec !== "string" && !common.isPlainObject(index.fieldOrSpec)) {
 						return Promise.reject(new ExtError("ERR_COLLECTION_INDEX_FIELDORSPEC_MUST_BE_OBJECTOR_STRING", "Collection index.fieldOrSpec must be plain object or string"));
 					}
-					return collection.createIndex(index.fieldOrSpec, fast.assign({collation: fast.object.clone(schema.options.collation || {})}, index.options || {}));
+					const options = fast.assign({}, index.options || {});
+					const collation = fast.object.clone(schema.options.collation || {});
+					if (!common.isEmpty(collation) && !options.collation) {
+						options.collation = collation;
+					}
+					return collection.createIndex(index.fieldOrSpec, common.isEmpty(options) ? undefined : options);
 				})));
 			});
 		}));
